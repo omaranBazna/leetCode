dfa0fc66428ca8f48a592e53b35efe3a3585884f
omaran bazna <omran4d@gmail.com>
2022-08-02 23:40:51 -0400

    finishin the pesudo code

--- problem.js
+++ problem.js
@@ -47,7 +47,9 @@ function checkCycle(map, edge, visited) {
   ///we need to check if we can reach the node2 to node1 from another path(cycle)
   ///for every element of the maping node2 if already visited ignore
   /// if not visited (if equal to node1 [not in the level of node2 first call]  return true)
-  /// if not equal to node1 add to visited check the cycle for this element
+  /// if not equal to node1 add to visited check the cycle for this element[recursive call ] before calling store temporary set
+  ///for what already visited
+  ///after the call re-set the already visited set to the temporary set
   /// if all node already visited and no one equal to the node1 return false
 }
 
